71180365f0bbd6505a10abe2e4c1d3eb02729933
FzzfSisyphus <60954024+FzzfSisyphus@users.noreply.github.com>
2023-11-16 23:59:52 +0800

    base fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Ped from '../pic/PedestrainDetection.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
